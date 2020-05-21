2e1317be2f3c8d896f30db54a2d7be906eadaa23
bbeyene <bbeyene@gmail.com>
2020-05-21 07:18:06 +0000

    done

---
+++ question1.py
@@ -0,0 +1,23 @@
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+import config
+
+""" Find the number of speeds > 100 in the data set. """
+
+ap = PlainTextAuthProvider(username=config.username, password=config.password)
+node_ips = config.hosts
+cluster = Cluster(node_ips, protocol_version=4, auth_provider=ap, port=config.port)
+session = cluster.connect('part_3_version_0')
+
+query = 'SELECT speed FROM loopdata_by_detector'
+statement = SimpleStatement(query, fetch_size=5000)
+
+count = 0
+for row in session.execute(statement):
+    if isinstance(row.speed, int):
+        count+=1
+
+print(count)
+cluster.shutdown()
+
